426d9e5e73af61ecc190b1e74cdcc0df3d84cd32
Ahmed Elmaghraby <a.elmaghraby@alumni.ubc.ca>
2015-11-20 16:53:16 -0800

    off-canvas menu is working

--- docs/examples/navbar-offcanvas/off-canvas.js
+++ docs/examples/navbar-offcanvas/off-canvas.js
@@ -1,4 +1,6 @@
       $( ".off-canvas" ).click(function() {
         $('body').toggleClass("off-canvas-pushed");
+        $('.off-canvas-menu').toggleClass("off-canvas-menu-pushed")
         $('.navbar-fixed-top').toggleClass("off-canvas-pushed");
+        
       });
